6b67e89a6edd44ce4359cedbec2c5092e1156c9d
Charles Eckman <charles.eckman@fastly.com>
2025-03-05 15:44:47 -0500

    the main code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use fastly::http::StatusCode;
+use fastly::http::{header, HeaderName, StatusCode};
 use fastly::{Error, Request, Response};
 
 #[fastly::main]
-fn main(_req: Request) -> Result<Response, Error> {
-    Ok(Response::from_status(StatusCode::OK))
+fn main(req: Request) -> Result<Response, Error> {
+    Ok(
+        if req.get_header(HeaderName::from_static("~^.^~")).is_some() {
+            Response::from_status(StatusCode::OK).with_body_text_html("I like your cat")
+        } else {
+            Response::from_status(StatusCode::TEMPORARY_REDIRECT)
+                .with_header(header::LOCATION, "https://http.cat")
+        },
+    )
 }
