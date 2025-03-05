7374230d53c9edcffc2b655c285518fbc24d96a9
Charles Eckman <charles.eckman@fastly.com>
2025-03-05 15:47:24 -0500

    newline

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use fastly::{Error, Request, Response};
 fn main(req: Request) -> Result<Response, Error> {
     Ok(
         if req.get_header(HeaderName::from_static("~^.^~")).is_some() {
-            Response::from_status(StatusCode::OK).with_body_text_html("I like your cat")
+            Response::from_status(StatusCode::OK).with_body_text_html("I like your cat\n")
         } else {
             Response::from_status(StatusCode::TEMPORARY_REDIRECT)
                 .with_header(header::LOCATION, "https://http.cat")
