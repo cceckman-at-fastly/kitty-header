ec847f6a8a9edbcf573f9ecb783287e6e423aa71
Charles Eckman <charles.eckman@fastly.com>
2025-04-16 10:33:06 -0400

    Upgrade, add a loopback for testing header size

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ use fastly::{Error, Request, Response};
 
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
-    Ok(
-        if req.get_header(HeaderName::from_static("~^.^~")).is_some() {
-            Response::from_status(StatusCode::OK).with_body_text_html("I like your cat\n")
-        } else {
-            Response::from_status(StatusCode::TEMPORARY_REDIRECT)
-                .with_header(header::LOCATION, "https://http.cat")
-        },
-    )
+    let resp = if req.get_header(HeaderName::from_static("~^.^~")).is_some() {
+        Response::from_status(StatusCode::OK).with_body_text_html("I like your cat\n")
+    } else {
+        Response::from_status(StatusCode::TEMPORARY_REDIRECT)
+            .with_header(header::LOCATION, "https://http.cat")
+    };
+
+    let resp = if let Some(h) = req.get_header(HeaderName::from_static("Too-Big")) {
+        resp.with_header(HeaderName::from_static("Too-Big"), h.len().to_string())
+    } else {
+        resp
+    };
+
+    Ok(resp)
 }
